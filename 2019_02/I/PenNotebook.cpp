18af65ddb997de5bf9e22c3b902c02d6380f4d2c
Charisk <2553036255@qq.com>
2021-03-13 23:56:10 +0800

    Other restruct

--- 2019_02/I/PenNotebook.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    int num, pen, note;
-    std::cin >> num >> pen >> note;
-    std::cout << ((pen >= num && note >= num)? "Yes" : "No" ) << std::endl;
-    return 0;
-}
