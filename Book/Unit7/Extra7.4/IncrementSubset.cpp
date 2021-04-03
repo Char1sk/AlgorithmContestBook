91002fff3a685e861616c099a3badd5e67282633
Charisk <2553036255@qq.com>
2021-04-03 21:13:05 +0800

    increament subset

--- Book/Unit7/Extra7.4/IncrementSubset.cpp
+++ Book/Unit7/Extra7.4/IncrementSubset.cpp
@@ -1,27 +1,43 @@
 #include <iostream>
 
-const int maxN = 5;
+const int maxN = 3;
 
 // [0, maxN-1] subsets
-void printSubset(int arr[maxN], int index)
+void printSubset(int arr[maxN], int out[maxN], int index)
 {
     for (int i = 0; i < index; ++i)
     {
-        printf("%d ", arr[i]);
+        printf("%d ", out[i]);
     }
     printf("\n");
     
-    int beg = index ? (arr[index-1]+1) : 0 ;
+    // can be optimized by Mapping
+    // to find the index of last value
+    int beg = 0;
+    if (index > 0)
+    {
+        int val = out[index-1];
+        for (int i = 0; i < maxN; ++i)
+        {
+            if (arr[i] == val)
+            {
+                beg = i+1;
+                break;
+            }
+        }
+    }
+    
     for (int i = beg; i < maxN; ++i)
     {
-        arr[index] = i;
-        printSubset(arr, index+1);
+        out[index] = arr[i];
+        printSubset(arr, out, index+1);
     }
 }
 
 int main()
 {
-    int arr[maxN];
-    printSubset(arr, 0);
+    int arr[maxN] = {1, 4, 5};
+    int out[maxN];
+    printSubset(arr, out, 0);
     return 0;
 }
