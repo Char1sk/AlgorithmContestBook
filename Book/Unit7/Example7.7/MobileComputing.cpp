93525c369f7f881e05c6923f79f3420fc6da5a66
Charisk <2553036255@qq.com>
2021-04-05 14:52:00 +0800

    i give up

--- Book/Unit7/Example7.7/MobileComputing.cpp
+++ Book/Unit7/Example7.7/MobileComputing.cpp
@@ -24,17 +24,31 @@ struct Node
 };
 
 Node *rt = nullptr;
+int maxW = -1;
 
-void *buildTree(Node *root, std::vector<int> &leafs, int &leafCount, int maxCount)
+void buildTree(Node *root, std::vector<int> &leafs, int &leafCount, int maxCount, int maxWid)
 {
     if (leafs.size() == 1)
     {
-        root = new Node(leafs[0]);
+        // root = new Node(leafs[0]);
         ++leafCount;
         if (leafCount == maxCount)
         {
-            rt->calWeight();
-            std::cout << rt->weight << std::endl;////
+            double ll = 0.0, rl = 0.0;
+            for (Node *p = rt; p != nullptr; p = p->left)
+            {
+                ll += p->leftLen;
+            }
+            for (Node *p = rt; p != nullptr; p = p->right)
+            {
+                rl += p->rightLen;
+            }
+            if (maxW < ll + rl && ll + rl < maxWid)
+            {
+                maxW = ll + rl;
+            }
+            // rt->calWeight();
+            // std::cout << rt->weight << std::endl;////
         }
     }
     else
@@ -43,20 +57,27 @@ void *buildTree(Node *root, std::vector<int> &leafs, int &leafCount, int maxCoun
         {
             int temp = leafCount;
             std::vector<int> lv, rv;
+            int lw = 0, rw = 0;
             for (int b = 0; b < leafs.size(); ++b)
             {
                 if (1&(s>>b))
                 {
                     lv.push_back(leafs[b]);
+                    lw += leafs[b];
                 }
                 else
                 {
                     rv.push_back(leafs[b]);
+                    rw += leafs[b];
                 }
             }
-            root = new Node();
-            buildTree(root->left, lv, leafCount, maxCount);
-            buildTree(root->right, rv, leafCount, maxCount);
+            
+            root = new Node(lw + rw);
+            root->leftLen = 1.0 * rw / (lw + rw);
+            root->rightLen = 1.0 * lw / (lw + rw);
+            
+            buildTree(root->left, lv, leafCount, maxCount, maxWid);
+            buildTree(root->right, rv, leafCount, maxCount, maxWid);
             
             // delete root;
             leafCount = temp;
@@ -82,16 +103,19 @@ int main()
     {
         double totalWid;
         int stoneCount;
-        std::vector<int> stones(stoneCount);
         std::cin >> totalWid >> stoneCount;
+        std::vector<int> stones(stoneCount, 0);
         for (int i = 0; i < stoneCount; ++i)
         {
+            // std::cout << stones.size() << std::endl;////
             std::cin >> stones[i];
         }
         
+        maxW = -1;
         rt = nullptr;
         int cnt = 0;
-        buildTree(rt, stones, cnt, stoneCount);
+        buildTree(rt, stones, cnt, stoneCount, totalWid);
+        std::cout << maxW << std::endl;
         clearTree(rt);
     }
     return 0;
